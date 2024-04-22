509107bca957691c8c63973b737db67dff504c67
Aftermath001 <alvinasiachi@gmail.com>
2024-04-22 14:57:53 +0300

    Fetch Photos data

--- my-app/src/pages/Photo/Photo.jsx
+++ my-app/src/pages/Photo/Photo.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import BreadCrumb from '../../components/Breadcrumb/Breadcrumb'
 import Meta from '../../components/Meta/Meta'
 import './Photo.scss'
 const Photo = () => {
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((photos) => setPhotos(photos));
+
+  }, []);
+   console.log(photos)
   return (
     <>
     <Meta title='PHOTO' />
@@ -13,7 +21,31 @@ const Photo = () => {
           <div className='col-3'>
           <div className='filter-card mb-3'>
                         <h3 className='filter-title'>PHOTOS PAGE</h3>
-                       
+                        <table>
+                          <thead style={{
+                            borderBottom: 'solid 3px blue',
+                            background: 'green',
+                            color: 'white',
+                            fontWeight: 'bold', }}>
+                            <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th>USERNAME</th>
+                                <th>PHONE</th>
+                            </tr>
+                            
+                          </thead>
+                          <tbody>
+                            {photos.map((photo) => (
+                              <tr key={photo.id}>
+                                <td>{photo.albumId}</td>
+                                <td>{photo.title}</td>
+                                <td>{photo.url}</td>
+                                <td>{photo.thumbnailUrl}</td>
+                              </tr>
+                            ))}
+                        </tbody>
+            </table>  
                     </div>
           </div>
           <div className='col-9'>
