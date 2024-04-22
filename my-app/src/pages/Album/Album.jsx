99a5f6b180793f3947da5305232ab90a73604186
Aftermath001 <alvinasiachi@gmail.com>
2024-04-22 13:02:25 +0300

    Fetch Album data

--- my-app/src/pages/Album/Album.jsx
+++ my-app/src/pages/Album/Album.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import './Album.scss'
 import BreadCrumb from '../../components/Breadcrumb/Breadcrumb'
 import Meta from '../../components/Meta/Meta'
 
 const Album = () => {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/albums")
+      .then((res) => res.json())
+      .then((albums) => setAlbums(albums));
+
+  }, []);
+  
+
   return (
     <>
-    <Meta title='ALBUM' />
-    <BreadCrumb title='ALBUM' />
+    <Meta title='ALBUMS' />
+    <BreadCrumb title='ALBUMS' />
     <div className='about-wrapper home-wrapper-2 py-5'>
       <div className='container-xxl'>
         <div className='row'>
           <div className='col-3'>
           <div className='filter-card mb-3'>
-                        <h3 className='filter-title'>ALBUMS PAGE</h3>
-                       
-                    </div>
+              <h3 className='filter-title'>ALBUMS PAGE</h3>
+              
+                <table>
+                  <thead style={{
+                    borderBottom: 'solid 3px blue',
+                    background: 'green',
+                    color: 'white',
+                    fontWeight: 'bold', }}>
+                    <tr>
+                        <th>ID</th>
+                        <th>TITLE</th>
+                    </tr>
+                    
+                  </thead>
+                  <tbody>
+                    {albums.map((album) => (
+                      <tr>
+                    
+                        {/* <td>{album.userId}</td> */}
+                        <td>{album.id}</td>
+                        <td>{album.title}</td>
+                      </tr>
+                ))}
+                </tbody>
+            </table>
+            
+          </div>
           </div>
           <div className='col-9'>
             <div className='row'>
