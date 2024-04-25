d290ff280c73de08f232821ad90dec6f43f1d04e
Aftermath001 <alvinasiachi@gmail.com>
2024-04-25 15:13:56 +0300

    User page Styling

--- my-app/src/pages/User/User.jsx
+++ my-app/src/pages/User/User.jsx
@@ -1,17 +1,26 @@
 // User.js
 import React, { useEffect, useState } from "react";
-import Card from "../../components/Card/Card";// Import the Card component
 import BreadCrumb from "../../components/Breadcrumb/Breadcrumb";
 import Meta from "../../components/Meta/Meta";
 
 import "./User.scss";
 const User = () => {
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 4;
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((users) => setUsers(users));
   }, []);
+
+  // Pagination Logic
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <>
@@ -20,24 +29,43 @@ const User = () => {
       <div className="about-wrapper home-wrapper-2 py-5">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-3">
-              <div className="filter-card mb-3">
-                <h3 className="filter-title">USER PAGE</h3>
-              </div>
-            </div>
-            <div className="col-9">
-              <div className="row">
-                {/* Display cards here */}
-                {users.map((user) => (
-                  <div key={user.id} className="col-md-4 mb-4">
-                    <Card user={user} />
+            {currentUsers.map((user) => (
+              <div key={user.id} className="col-6">
+                <div className="card user-card">
+                  <div className="card-header">
+                    <h5 className="card-title">NAME: {user.name}</h5>
                   </div>
-                ))}
+                  <div className="card-body">
+                    <p>User ID: {user.id}</p>
+                    <p>Username: {user.username}</p>
+                    <p>Email: {user.email}</p>
+                    {/* Add other user data as needed */}
+                    <button className="button edit-button">Edit</button>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
+      {/* Pagination */}
+      <nav>
+        <ul className="pagination">
+          {Array.from(
+            { length: Math.ceil(users.length / usersPerPage) },
+            (_, i) => (
+              <li key={i} className="page-item">
+                <button
+                  onClick={() => paginate(i + 1)}
+                  className="page-link"
+                >
+                  {i + 1}
+                </button>
+              </li>
+            )
+          )}
+        </ul>
+      </nav>
     </>
   );
 };
