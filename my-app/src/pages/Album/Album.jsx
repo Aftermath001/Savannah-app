b0a63df5d4fde20f00f6e3dda56a4b21f83dbf61
Aftermath001 <alvinasiachi@gmail.com>
2024-04-26 09:53:57 +0300

    Display Selected-Album Info

--- my-app/src/pages/Album/Album.jsx
+++ my-app/src/pages/Album/Album.jsx
@@ -1,11 +1,12 @@
 // Album.js
 import React, { useEffect, useState } from "react";
-import './Album.scss'
-import BreadCrumb from '../../components/Breadcrumb/Breadcrumb'
-import Meta from '../../components/Meta/Meta'
+import BreadCrumb from "../../components/Breadcrumb/Breadcrumb";
+import Meta from "../../components/Meta/Meta";
+import "./Album.scss";
 
 const Album = () => {
   const [albums, setAlbums] = useState([]);
+  const [selectedAlbum, setSelectedAlbum] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const albumsPerPage = 10;
 
@@ -15,6 +16,10 @@ const Album = () => {
       .then((albums) => setAlbums(albums));
   }, []);
 
+  const handleAlbumClick = (album) => {
+    setSelectedAlbum(album);
+  };
+
   const indexOfLastAlbum = currentPage * albumsPerPage;
   const indexOfFirstAlbum = indexOfLastAlbum - albumsPerPage;
   const currentAlbums = albums.slice(indexOfFirstAlbum, indexOfLastAlbum);
@@ -23,14 +28,14 @@ const Album = () => {
 
   return (
     <>
-      <Meta title='ALBUMS' />
-      <BreadCrumb title='ALBUMS' />
-      <div className='album-wrapper py-5'>
-        <div className='container-xxl'>
-          <div className='row'>
-            <div className='col-3'>
-              <div className='filter-card mb-3'>
-                <h3 className='filter-title'>ALBUMS PAGE</h3>
+      <Meta title="ALBUMS" />
+      <BreadCrumb title="ALBUMS" />
+      <div className="album-wrapper py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">ALBUMS PAGE</h3>
                 {/* Move pagination here */}
                 <nav>
                   <ul className="pagination">
@@ -51,16 +56,20 @@ const Album = () => {
                 </nav>
               </div>
             </div>
-            <div className='col-9'>
-              <div className='row'>
+            <div className="col-9">
+              <div className="row">
                 {/* Render album cards */}
                 {currentAlbums.map((album) => (
-                  <div key={album.id} className='col-6'>
-                    <div className='card album-card'>
-                      <div className='card-header'>
-                        <h5 className='card-title'>ALBUM ID: {album.id}</h5>
+                  <div
+                    key={album.id}
+                    className="col-6"
+                    onClick={() => handleAlbumClick(album)}
+                  >
+                    <div className="card album-card">
+                      <div className="card-header">
+                        <h5 className="card-title">ALBUM ID: {album.id}</h5>
                       </div>
-                      <div className='card-body'>
+                      <div className="card-body">
                         <p>USER ID: {album.userId}</p>
                         <p>TITLE: {album.title}</p>
                       </div>
@@ -72,8 +81,17 @@ const Album = () => {
           </div>
         </div>
       </div>
+      {/* Display selected album info */}
+      {selectedAlbum && (
+        <div className="selected-album-info">
+          <h2>Album Title:{selectedAlbum.title}</h2>
+          <p>Album ID: {selectedAlbum.id}</p>
+          <p>User ID: {selectedAlbum.userId}</p>
+          {/* <p>Album Title: {selectedAlbum.}</p> */}
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default Album;
